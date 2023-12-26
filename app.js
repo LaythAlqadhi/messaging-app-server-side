491972adf73beca269d76cbff983f291d185d32e
LaythAlqadhi <125077045+LaythAlqadhi@users.noreply.github.com>
2023-12-26 17:13:53 +0000

    Add Passport authentication and MongoDB connection and organize the template

--- app.js
+++ app.js
@@ -3,37 +3,87 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const RateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const compression = require('compression');
+const cors = require('cors');
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
 
+// Connect to MongoDB
+require('./database/mongoConfig');
+
+// Define routes
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
+// Define models
+const User = require('./models/user');
+
+// Create Express app
 const app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Rate limiting
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 20,
+});
+
+// JWT strategy configuration
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+// Passport configuration
+passport.use(
+  new JwtStrategy(jwtOptions, async (jwtPayload, done) => {
+    try {
+      const user = await User.findById(jwtPayload.sub).exec();
+
+      if (user) {
+        return done(null, user);
+      }
+      return done(null, false);
+    } catch (err) {
+      return done(err, false);
+    }
+  }),
+);
+
+// Apply Express middleware
+app.use(limiter);
+app.use(passport.initialize());
+app.use(helmet());
+app.use(compression());
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Define routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
